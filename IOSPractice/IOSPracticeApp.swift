b4a0c369a3d0639c7d899911528aa6259b931c41
Tanveer Singh <tanveerbadhan@gmail.com>
2024-10-22 18:34:16 -0400

    Initial Commit

---
+++ IOSPractice/IOSPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSPracticeApp.swift
+//  IOSPractice
+//
+//  Created by Tanveer Singh on 2024-10-22.
+//
+
+import SwiftUI
+
+@main
+struct IOSPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IOSPracticeTests/IOSPracticeTests.swift
@@ -0,0 +1,17 @@
+//
+//  IOSPracticeTests.swift
+//  IOSPracticeTests
+//
+//  Created by Tanveer Singh on 2024-10-22.
+//
+
+import Testing
+@testable import IOSPractice
+
+struct IOSPracticeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
